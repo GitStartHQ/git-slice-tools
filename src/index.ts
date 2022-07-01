8a92fde08cf6ee8e0f4587395d893dd59b5a1277
phunguyenmurcul <phu.nguyen@murcul.tech>
2022-07-01 15:45:13 +0700

    feat: get pr title/body from slice PR

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,6 @@ const argv = yargs(process.argv.slice(2))
     .options({
         action: { type: 'string', choices: ['pull', 'push', 'checkout', 'raise-pr'], alias: 'a' },
         branch: { type: 'string', alias: 'b' },
-        title: { type: 'string', alias: 't' },
-        description: { type: 'string', alias: 'd' },
         message: { type: 'string', alias: 'm' },
         forcePush: { type: 'boolean', alias: 'force-push', default: false },
     })
@@ -42,15 +40,7 @@ init(actionInputs).then(({ sliceGit, upstreamGit }) => {
                 throw new Error(`raise-pr job: 'branch' in string is required`)
             }
 
-            if (!argv.title || typeof argv.title !== 'string') {
-                throw new Error(`raise-pr job: 'title' in string is required`)
-            }
-
-            if (!argv.description || typeof argv.description !== 'string') {
-                throw new Error(`raise-pr job: 'description' in string is required`)
-            }
-
-            return raisePr(actionInputs, argv.branch, argv.title, argv.description)
+            return raisePr(actionInputs, argv.branch)
         }
         default: {
             return
